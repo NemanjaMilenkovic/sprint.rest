800813f92a1fb1147b57de595040a021c1791f13
akekona8 <ashley.kekona@gmail.com>
2020-01-21 15:18:52 +0900

    implemented up to Get attacks

--- src/server.js
+++ src/server.js
@@ -2,15 +2,33 @@
 const pokeData = require("./data");
 const express = require("express");
 
-// console.log("---------->", pokeData.attacks);
 /**
  * Create, set up and return your express server, split things into separate files if it becomes too long!
  */
 
 const setupServer = () => {
   const pokemon = pokeData.pokemon;
+  const types = pokeData.types;
+  const attacks = pokeData.attacks;
   const app = express();
   app.use(express.json());
+
+  function getPoke(idOrName) {
+    let returnPoke;
+    if (parseInt(idOrName.idOrName)) {
+      if (pokemon[parseInt(idOrName.idOrName) - 1]) {
+        returnPoke = pokemon[parseInt(idOrName.idOrName) - 1];
+      }
+    } else {
+      idOrName = idOrName.idOrName.toLowerCase();
+      for (const poke of pokemon) {
+        if (poke.name.toLowerCase() === idOrName) {
+          returnPoke = poke;
+        }
+      }
+    }
+    return returnPoke;
+  }
 
   app.get("/api/pokemon/", (req, res) => {
     const limit = req.query.limit;
@@ -34,49 +52,112 @@ const setupServer = () => {
   });
 
   app.get("/api/pokemon/:idOrName/", (req, res) => {
+    const idOrName = req.params;
+    res.send(getPoke(idOrName));
+  });
+
+  app.patch("/api/pokemon/:idOrName/", (req, res) => {
     let returnPoke;
     const idOrName = req.params;
     const update = req.query.update;
-    const remove = req.query.remove;
 
     if (idOrName !== undefined && update !== undefined) {
       const updateName = update.split(",")[0];
       const updateValue = update.split(",")[1];
-      getPoke(idOrName);
+      returnPoke = getPoke(idOrName);
 
       if (returnPoke.hasOwnProperty(updateName)) {
         returnPoke[updateName] = updateValue;
       }
-    } else if (idOrName !== undefined && remove !== undefined) {
-      removePoke(idOrName);
-      res.send(pokemon);
-      return;
     } else {
-      getPoke(idOrName);
+      returnPoke = getPoke(idOrName);
     }
 
-    function removePoke(idOrName) {
-      getPoke(idOrName);
-      console.log("pokemon.length :", pokemon.length);
-      pokemon.splice(pokemon.indexOf(returnPoke), 1);
-      console.log("pokemon.length :", pokemon.length);
-    }
-
-    function getPoke(idOrName) {
-      if (parseInt(idOrName.idOrName)) {
-        if (pokemon[parseInt(idOrName.idOrName) - 1]) {
-          returnPoke = pokemon[parseInt(idOrName.idOrName) - 1];
-        }
-      } else {
-        idOrName = idOrName.idOrName.toLowerCase();
-        for (const poke of pokemon) {
-          if (poke.name.toLowerCase() === idOrName) {
-            returnPoke = poke;
-          }
-        }
-      }
-    }
     res.send(returnPoke);
+  });
+
+  app.delete("/api/pokemon/:idOrName/", (req, res) => {
+    let returnPoke;
+    const idOrName = req.params;
+    const remove = req.query.remove;
+
+    if (idOrName !== undefined && remove !== undefined) {
+      removePoke(idOrName);
+    }
+    function removePoke(idOrName) {
+      returnPoke = getPoke(idOrName);
+      pokemon.splice(pokemon.indexOf(returnPoke), 1);
+    }
+    res.send(pokemon);
+  });
+
+  app.get("/api/pokemon/:idOrName/evolutions", (req, res) => {
+    const idOrName = req.params;
+    const returnPoke = getPoke(idOrName);
+    if (!returnPoke.evolutions) {
+      res.send([]);
+    } else {
+      res.send(returnPoke.evolutions);
+    }
+  });
+
+  app.get("/api/pokemon/:idOrName/evolutions/previous", (req, res) => {
+    const idOrName = req.params;
+    const returnPoke = getPoke(idOrName);
+    if (!returnPoke["Previous evolution(s)"]) {
+      res.send([]);
+    } else {
+      res.send(returnPoke["Previous evolution(s)"]);
+    }
+  });
+
+  app.get("/api/types", (req, res) => {
+    const limit = req.query.limit;
+    if (limit === undefined) {
+      res.send(types);
+    } else {
+      res.send(types.slice(0, limit));
+    }
+  });
+
+  app.post("/api/types", (req, res) => {
+    let add = req.query.add;
+    add = add.charAt(0).toUpperCase() + add.substring(1).toLowerCase();
+    types.push(add);
+    res.send(types);
+  });
+
+  app.delete("/api/types/:name", (req, res) => {
+    let name = req.params;
+    name =
+      name.name.charAt(0).toUpperCase() + name.name.substring(1).toLowerCase();
+    const indexOfName = types.indexOf(name);
+    if (indexOfName >= 0) {
+      types.splice(indexOfName, 1);
+    }
+    res.send(types);
+  });
+
+  app.get("/api/types/:type/pokemon", (req, res) => {
+    let type = req.params;
+    type =
+      type.type.charAt(0).toUpperCase() + type.type.substring(1).toLowerCase();
+    const pokeByType = pokemon
+      .filter((poke) => {
+        return poke.types.includes(type);
+      })
+      .map((poke) => ({ name: poke.name, id: poke.id }));
+
+    res.send(pokeByType);
+  });
+
+  app.get("/api/attacks", (req, res) => {
+    const limit = req.query.limit;
+    if (limit === undefined) {
+      res.send(attacks);
+    } else {
+      res.send(attacks.slice(0, limit));
+    }
   });
 
   return app;
--- test/_pokemonApi.js
+++ test/_pokemonApi.js
@@ -59,15 +59,109 @@ describe("Pokemon API Server", () => {
   });
   describe("PATCH /api/pokemon/:idOrName", () => {
     it("should allow modifications", async () => {
-      const res = await request.get("/api/pokemon/Golbat/?update=maxCP,1478");
+      const res = await request.patch("/api/pokemon/Golbat/?update=maxCP,1478");
       JSON.parse(res.text).maxCP.should.equal("1478");
     });
   });
   describe("DELETE /api/pokemon/:idOrName", () => {
     it("should remove a Pokemon from the array", async () => {
       const oldLength = pokeData.pokemon.length;
-      const res = await request.get("/api/pokemon/Golbat/?remove=Golbat");
+      const res = await request.delete("/api/pokemon/Golbat/?remove=Golbat");
       JSON.parse(res.text).length.should.equal(oldLength - 1);
+    });
+  });
+  describe("GET /api/pokemon/:idOrName/evolutions", () => {
+    it("should return the evolutions a Pokemon has", async () => {
+      const bulbasaurEvo = [
+        {
+          id: 2,
+          name: "Ivysaur",
+        },
+        {
+          id: 3,
+          name: "Venusaur",
+        },
+      ];
+      const res = await request.get("/api/pokemon/Bulbasaur/evolutions");
+      JSON.parse(res.text).should.deep.equal(bulbasaurEvo);
+    });
+    it("should return an empty array if Pokemon doesn't have evolutions", async () => {
+      const res = await request.get("/api/pokemon/Venusaur/evolutions");
+      JSON.parse(res.text).should.deep.equal([]);
+    });
+  });
+  describe("GET /api/pokemon/:idOrName/evolutions/previous", () => {
+    it("should return the previous evolutions a Pokemon has", async () => {
+      const venusaurPreEvo = [
+        {
+          id: 1,
+          name: "Bulbasaur",
+        },
+        {
+          id: 2,
+          name: "Ivysaur",
+        },
+      ];
+      const res = await request.get(
+        "/api/pokemon/Venusaur/evolutions/previous"
+      );
+      JSON.parse(res.text).should.deep.equal(venusaurPreEvo);
+    });
+    it("should return an empty array if Pokemon doesn't have evolutions", async () => {
+      const res = await request.get(
+        "/api/pokemon/Bulbasaur/evolutions/previous"
+      );
+      JSON.parse(res.text).should.deep.equal([]);
+    });
+  });
+  describe("GET /api/types", () => {
+    it("should return a full list of available types", async () => {
+      const res = await request.get("/api/types");
+      JSON.parse(res.text).should.deep.equal(pokeData.types);
+    });
+    it("should take a query parameter limit=n", async () => {
+      const res = await request.get("/api/types?limit=2");
+      JSON.parse(res.text).length.should.equal(2);
+    });
+  });
+  describe("POST /api/types", () => {
+    it("should add a type", async () => {
+      const oldLength = pokeData.types.length;
+      const res = await request.post("/api/types/?add=Iron");
+      JSON.parse(res.text).length.should.equal(oldLength + 1);
+    });
+  });
+  describe("DELETE /api/types/:name", () => {
+    it("should delete a given type", async () => {
+      const oldLength = pokeData.types.length;
+      const res = await request.delete("/api/types/Bug");
+      JSON.parse(res.text).length.should.equal(oldLength - 1);
+    });
+    it("should return the array if the type is not found", async () => {
+      const res = await request.delete("/api/types/Bug");
+      JSON.parse(res.text).should.deep.equal(pokeData.types);
+    });
+  });
+  describe("GET /api/types/:type/pokemon", () => {
+    it("should return all Pokemon of a given type", async () => {
+      const firePoke = [];
+      for (const poke of pokeData.pokemon) {
+        if (poke.types.includes("Fire")) {
+          firePoke.push({ name: poke.name, id: poke.id });
+        }
+      }
+      const res = await request.get("/api/types/Fire/pokemon");
+      JSON.parse(res.text).should.deep.equal(firePoke);
+    });
+  });
+  describe("GET /api/attacks", () => {
+    it("should return a full list of available attacks", async () => {
+      const res = await request.get("/api/attacks");
+      JSON.parse(res.text).should.deep.equal(pokeData.attacks);
+    });
+    it("should take a query parameter limit=n", async () => {
+      const res = await request.get("/api/types?limit=2");
+      JSON.parse(res.text).length.should.equal(2);
     });
   });
 });
